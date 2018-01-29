ad40835414cad16e7178d205a284669650e7b349
Meaghan Cox <mkcox@umich.edu>
2018-01-29 14:12:16 -0500

    Adding check question code

--- hw4_8ball.py
+++ hw4_8ball.py
@@ -1,1 +1,10 @@
 #We're making a magic 8-ball game!
+
+# check_question
+question = input("Ask me a question and I will tell you your future: ")
+while True:
+	if question[-1] != "?":
+		print("I'm sorry, I can only answer questions.")
+		question = input("Ask me a question and I will tell you your future: ")
+	else:
+		break
