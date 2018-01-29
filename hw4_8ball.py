212c0b66fd09fb5cd47df54987eb6989397252c0
p1ho <hopokuan@gmail.com>
2018-01-29 14:52:41 -0500

    "Merging 2 answer lists for the main 8ball game."

--- hw4_8ball.py
+++ hw4_8ball.py
@@ -1,14 +1,13 @@
 #We're making a magic 8-ball game!
-
+import random
 # check_question
-# question = input("Ask me a question and I will tell you your future: ")
 while True:
 	question = input("Ask me a question and I will tell you your future: ")
 	if question[-1] != "?":
 		print("I'm sorry, I can only answer questions.")
 	else:
 		break
-answer_list_A = ["It is certain",
+answers = ["It is certain",
             "It is decidedly so",
             "Without a doubt",
             "Yes definitely",
@@ -19,7 +18,11 @@ answer_list_A = ["It is certain",
             "Yes",
             "Signs point to yes"]
 # Person C, Yuanting Lin, adds the last 10 answers. 
-answers = ["Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good"]
+answers_B = ["Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good"]
+
+# Concatenate answers
+for item in answers_B:
+    answers.append(item)
 
 answer = random.choice(answers)
 print(answer)
