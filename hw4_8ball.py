22c0b2d70026904616bedfdc2eb5b0c91d66cebe
p1ho <hopokuan@gmail.com>
2018-01-29 15:04:59 -0500

    Added comments to specify who did what.

--- hw4_8ball.py
+++ hw4_8ball.py
@@ -1,12 +1,14 @@
 #We're making a magic 8-ball game!
 import random
 # check_question
+# Person A, Meaghan Cox, adds the user input prompt
 while True:
 	question = input("Ask me a question and I will tell you your future: ")
 	if question[-1] != "?":
 		print("I'm sorry, I can only answer questions.")
 	else:
 		break
+# Person B, Pokuan Ho, adds the first 10 answers.
 answers = ["It is certain",
             "It is decidedly so",
             "Without a doubt",
@@ -20,7 +22,7 @@ answers = ["It is certain",
 # Person C, Yuanting Lin, adds the last 10 answers. 
 answers_B = ["Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good"]
 
-# Concatenate answers
+# Concatenate answers from Person B and C
 for item in answers_B:
     answers.append(item)
 
