0fc37ab5548d838933ee5f894000f4f5f9aa4c14
p1ho <hopokuan@gmail.com>
2018-01-29 13:50:31 -0500

    "Added list containing first 10 questions for Magic 8 ball"

--- hw4_8ball.py
+++ hw4_8ball.py
@@ -1,1 +1,11 @@
 #We're making a magic 8-ball game!
+answer_list_A = ["It is certain",
+            "It is decidedly so",
+            "Without a doubt",
+            "Yes definitely",
+            "You may rely on it",
+            "As I see it, yes",
+            "Most likely",
+            "Outlook good",
+            "Yes",
+            "Signs point to yes"]
